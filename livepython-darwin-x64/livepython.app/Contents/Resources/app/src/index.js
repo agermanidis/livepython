45a92768615953ae3bc602b7fe7686baceba591e
Anastasis Germanidis <agermanidis@gmail.com>
2017-10-06 15:10:16 -0400

    don't use packaged version for now

--- livepython-darwin-x64/livepython.app/Contents/Resources/app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import MainView from './components/MainView'
-
-ReactDOM.render(
-    <MainView />,
-    document.getElementById('root')
-)
